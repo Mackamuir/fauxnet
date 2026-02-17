1994f4712c628b9882ee384bf0af80dd6daa04fd
mackamuir <mackamuir@gmail.com>
2026-02-17 12:02:27 +1100

    Update fauxnet@webui.service and vite.config.js for improved build and preview commands

--- webui/frontend/vite.config.js
+++ webui/frontend/vite.config.js
@@ -12,5 +12,15 @@ export default defineConfig({
         changeOrigin: true,
       }
     }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 80,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      }
+    }
   }
 })
